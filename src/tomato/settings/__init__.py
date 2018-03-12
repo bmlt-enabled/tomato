dd670a568f88c2e7a8ac99ef1dec257e4a50047c
jbraswell <jbraswell@localhost.localdomain>
2018-03-11 20:32:37 -0400

    Do not run with DEBUG = True in production

--- src/tomato/settings/__init__.py
+++ src/tomato/settings/__init__.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '=x4-vt4g35xraoleu&b6i-qfy+)ku!^mk^5i!ju!9mjb0gp8sm')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
