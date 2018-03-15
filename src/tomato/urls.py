024b63efff2b89c221a02da4484bcccdba546424
jbraswell <jbraswell@localhost.localdomain>
2018-03-15 00:24:34 -0400

    Remove sandwich URLs

--- src/tomato/urls.py
+++ src/tomato/urls.py
@@ -8,41 +8,10 @@ from .api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    url(r'_/sandwich/client_interface/xml/GetServiceBodies.php$',
-        views.get_service_bodies_php,
-        name='get-service-bodies-php-sandwich'),
-    url(r'main_server[/]+client_interface/xml/GetServiceBodies.php$',
-        views.get_service_bodies_php,
-        name='get-service-bodies-php'),
-
-    url(r'_/sandwich/client_interface/(?P<format>json|xml)/GetLangs.php$',
-        views.get_langs_php,
-        name='get-langs-php-sandwich'),
-    url(r'main_server[/]+client_interface/(?P<format>json|xml)/GetLangs.php$',
-        views.get_langs_php,
-        name='get-langs-php'),
-
-    url(r'_/sandwich/client_interface/(?P<format>json|csv|xml)/',
-        views.semantic_query,
-        name='semantic-query-sandwich'),
-    url(r'main_server[/]+client_interface/(?P<format>json|csv|xml)/',
-        views.semantic_query,
-        name='semantic-query'),
-
-    url(r'_/sandwich/client_interface/serverInfo.xml$',
-        views.server_info_xml,
-        name='server-info-xml-sandwich'),
-    url(r'main_server[/]+client_interface/serverInfo.xml$',
-        views.server_info_xml,
-        name='server-info-xml'),
-
-    url(r'_/sandwich/client_interface/xsd/(?P<schema_name>GetSearchResults|GetFormats)\.php$',
-        views.xsd,
-        name='xsd-sandwich'),
-    url(r'main_server[/]+client_interface/xsd/(?P<schema_name>GetSearchResults|GetFormats)\.php$',
-        views.xsd,
-        name='xsd'),
-
+    url(r'main_server[/]+client_interface/xml/GetServiceBodies.php$', views.get_service_bodies_php, name='get-service-bodies-php'),
+    url(r'main_server[/]+client_interface/(?P<format>json|xml)/GetLangs.php$', views.get_langs_php, name='get-langs-php'),
+    url(r'main_server[/]+client_interface/(?P<format>json|csv|xml)/', views.semantic_query, name='semantic-query'),
+    url(r'main_server[/]+client_interface/serverInfo.xml$', views.server_info_xml, name='server-info-xml'),
+    url(r'main_server[/]+client_interface/xsd/(?P<schema_name>GetSearchResults|GetFormats)\.php$', views.xsd, name='xsd'),
     url(r'^%s/(?P<path>.*)$' % re.escape(settings.STATIC_URL.strip('/')), serve, kwargs={'insecure': True}),
 ]
