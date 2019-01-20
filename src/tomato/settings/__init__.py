54646966eb4b06668df3c13a2c33513a215bfb76
Jonathan Braswell <jonathan@f9teams.com>
2019-01-19 22:47:02 -0500

    Don;'t import duplicate data from nj server

--- src/tomato/settings/__init__.py
+++ src/tomato/settings/__init__.py
@@ -187,5 +187,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 IGNORE_SERVICE_BODIES = {
     'https://bmlt.sezf.org/main_server/': [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 19, 33, 34, 35, 36, 38],
     'https://naflorida.org/bmlt_server/': [9, 18, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 37, 39, 41],
-    'http://naflorida.org/bmlt_server/': [9, 18, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 37, 39, 41]
+    'http://naflorida.org/bmlt_server/': [9, 18, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 37, 39, 41],
+    'https://www.narcoticsanonymousnj.org/main_server/': [31, 32],  # Duplicated Eastern New York Region
 }
