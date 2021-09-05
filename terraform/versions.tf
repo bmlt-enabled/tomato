e702177a9d78a7a7fd0c97827f1cfbaf23dbcd62
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2021-09-04 22:48:50 -0400

    fix typo in aws provider version pin

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.657"
+      version = "~> 3.57"
     }
     template = {
       source  = "hashicorp/template"
