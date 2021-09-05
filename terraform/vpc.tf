e5268188b8821cb5e3dcaa1b72ac9b6b16bffd3f
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2021-09-04 20:49:52 -0400

    adding nat gatewy (#118)
    make private subnets private

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -30,11 +30,78 @@ resource "aws_subnet" "public_b" {
   }
 }
 
+resource "aws_subnet" "private_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "tomato-private-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 3)
+  availability_zone       = "us-east-1b"
+
+  tags = {
+    Name = "tomato-private-b"
+  }
+}
+
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "tomato"
+  }
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name = "tomato"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_b.id
+
+  tags = {
+    Name = "tomato"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "tomato-private"
+  }
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main.id
+
+  timeouts {
+    create = "5m"
   }
 }
 
@@ -61,4 +128,3 @@ resource "aws_route" "main_subnets_to_internet_gateway" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
-
