f392114429700053835235cdc36759a01d922c44
pjaudiomv <34245618+pjaudiomv@users.noreply.github.com>
2022-11-28 19:53:23 -0500

    bump aws provider to 4.x (#143)

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 1.3.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.57"
+      version = "~> 4.40"
     }
     template = {
       source  = "hashicorp/template"
