dd5c4633c0316b6f44ea38d0172292288c71cb04
Jonathan Braswell <10187286+jbraswell@users.noreply.github.com>
2022-12-06 11:31:40 -0500

    remove deprecated python code

--- src/tomato/test_runner.py
+++
@@ -1,8 +0,0 @@
-import logging
-from django.test.runner import DiscoverRunner
-
-
-class DisableLogsTestRunner(DiscoverRunner):
-    def run_tests(self, test_labels, extra_tests=None, **kwargs):
-        logging.disable(logging.CRITICAL)
-        return super().run_tests(test_labels, extra_tests, **kwargs)
