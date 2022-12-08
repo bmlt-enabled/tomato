805d7f81fa37bd3e6fe9956c6142829267c131c9
Patrick Joyce <pjaudiomv@gmail.com>
2022-12-08 15:43:28 -0500

    update aggregator terraform

--- terraform/.terraform.lock.hcl
+++ terraform/.terraform.lock.hcl
@@ -1,44 +1,44 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
-provider "registry.terraform.io/hashicorp/archive" {
-  version = "2.2.0"
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "4.45.0"
+  constraints = "~> 4.40"
   hashes = [
-    "h1:2K5LQkuWRS2YN1/YoNaHn9MAzjuTX8Gaqy6i8Mbfv8Y=",
-    "zh:06bd875932288f235c16e2237142b493c2c2b6aba0e82e8c85068332a8d2a29e",
-    "zh:0c681b481372afcaefddacc7ccdf1d3bb3a0c0d4678a526bc8b02d0c331479bc",
-    "zh:100fc5b3fc01ea463533d7bbfb01cb7113947a969a4ec12e27f5b2be49884d6c",
-    "zh:55c0d7ddddbd0a46d57c51fcfa9b91f14eed081a45101dbfc7fd9d2278aa1403",
-    "zh:73a5dd68379119167934c48afa1101b09abad2deb436cd5c446733e705869d6b",
-    "zh:841fc4ac6dc3479981330974d44ad2341deada8a5ff9e3b1b4510702dfbdbed9",
-    "zh:91be62c9b41edb137f7f835491183628d484e9d6efa82fcb75cfa538c92791c5",
-    "zh:acd5f442bd88d67eb948b18dc2ed421c6c3faee62d3a12200e442bfff0aa7d8b",
-    "zh:ad5720da5524641ad718a565694821be5f61f68f1c3c5d2cfa24426b8e774bef",
-    "zh:e63f12ea938520b3f83634fc29da28d92eed5cfbc5cc8ca08281a6a9c36cca65",
-    "zh:f6542918faa115df46474a36aabb4c3899650bea036b5f8a5e296be6f8f25767",
+    "h1:ZFb6RqY48Fe+18sOC62wiE38XYPaTg98RJZO+EnsvCU=",
+    "zh:22da03786f25658a000d1bcc28c780816a97e7e8a1f59fff6eee7d452830e95e",
+    "zh:2543be56eee0491eb0c79ca1c901dcbf71da26625961fe719f088263fef062f4",
+    "zh:31a1da1e3beedfd88c3c152ab505bdcf330427f26b75835885526f7bb75c4857",
+    "zh:4409afe50f225659d5f378fe9303a45052953a1219f7f1acc82b69d07528b7ba",
+    "zh:4dadec3b783f10d2f8eef3dab5e817baae9c932a7967d45fe3d77fcbcbdaa438",
+    "zh:55be80d6e24828dcb0db7a0226fb275415c1c0ad63dd2f33b76f3ac0cd64e6a6",
+    "zh:560bba29efb7dbe0bfcc937369d88817aa31a8d18aa25395b1afe2576cb04495",
+    "zh:6caacc202e83438ff63d5d96733e283f44e349668d96c6b1c5c7df463ebf85cc",
+    "zh:6cabab83a61d5b4ac801c5a5d57556a0e76ec8dc879d28cf777509db5f6a657e",
+    "zh:96c4528bf9c16edb8841b68479ec51c499ed7fa680462fa28caeab3fc168bb43",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:cdc0b47ff840d708fbf75abfe86d23dc7f1dffdd233a771822a17b5c637f4769",
+    "zh:d9a9583e82776d1ebb6cf6c3d47acc2b302f8778f470ceffe7579dc794eb1feb",
+    "zh:e9367ca9f6f6418a23cdf8d01f29dd0c4f614e78499f52a767a422e4c334b915",
+    "zh:f6d355a2fb3bcebb597f68bbca4fa2aaa364efd29240236c582375e219d77656",
   ]
 }
 
-provider "registry.terraform.io/hashicorp/aws" {
-  version     = "4.42.0"
-  constraints = "~> 4.40"
+provider "registry.terraform.io/hashicorp/cloudinit" {
+  version = "2.2.0"
   hashes = [
-    "h1:cS7q80JomJrUZpm+bnK5H/iRjF5+7HAA3qgw+JznPiM=",
-    "zh:091b64bccee701462b19ca99fe3bff0716e9445a88d0e4d0d0f322062b02bb60",
-    "zh:1fd9b0bf3421ad65284d693e60de068fc9b247d4fa7df6c1d62ad4796088f795",
-    "zh:3e34e4fcfaa30b04811aaa92c4d6115ddaa820ac11fa82ad217f42ae17a068ea",
-    "zh:47b412ab9cc3730797659ffb775a429b5398a5f403c9cca2ec5f663e21a69077",
-    "zh:8e29e90fdf29d76bb8fab62c184c4ec78e37030277dbe2c0dd97557fdfcbcb50",
-    "zh:8ef4e94b5672234a68649bdbb93416c1829c5cd6f37be584f8e8610f14ca95b0",
-    "zh:92a3eb5ae0c2c83717973c56b0427bf1fe8fba3ba72ced01e5eefc5c0cff8bf3",
-    "zh:96b9f714aed24206f8f47af39426aa8c02f172ac6d5516bcc375583d120bf4f8",
-    "zh:996ec2065cf0c52b125e3ac8bbd059e5733d8393143cebbb427a236d08c742b4",
-    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
-    "zh:b3abba32ccbea87b6a46e846c878ca0b2e9736dbaedd512a712398ece91de431",
-    "zh:b77292051d499f66ed80434ac435930204b92ca906c0d38fdb8a0ac37efa25ae",
-    "zh:f4ed19b15bd7cd99ee248023a94a10d586e69f81d0d8326d87a79cc37b579a4f",
-    "zh:f906a8003e6ad0dd561d8c62e02a65835d8c5009dd7cbfe03e28becced82e5db",
-    "zh:faff211d1559cbae669b63cdd6436a2ef0fb24108d8fa73b625dc5334b50aada",
+    "h1:siiI0wK6/jUDdA5P8ifTO0yc9YmXHml4hz5K9I9N+MA=",
+    "zh:76825122171f9ea2287fd27e23e80a7eb482f6491a4f41a096d77b666896ee96",
+    "zh:795a36dee548e30ca9c9d474af9ad6d29290e0a9816154ad38d55381cd0ab12d",
+    "zh:9200f02cb917fb99e44b40a68936fd60d338e4d30a718b7e2e48024a795a61b9",
+    "zh:a33cf255dc670c20678063aa84218e2c1b7a67d557f480d8ec0f68bc428ed472",
+    "zh:ba3c1b2cd0879286c1f531862c027ec04783ece81de67c9a3b97076f1ce7f58f",
+    "zh:bd575456394428a1a02191d2e46af0c00e41fd4f28cfe117d57b6aeb5154a0fb",
+    "zh:c68dd1db83d8437c36c92dc3fc11d71ced9def3483dd28c45f8640cfcd59de9a",
+    "zh:cbfe34a90852ed03cc074601527bb580a648127255c08589bc3ef4bf4f2e7e0c",
+    "zh:d6ffd7398c6d1f359b96f5b757e77b99b339fbb91df1b96ac974fe71bc87695c",
+    "zh:d9c15285f847d7a52df59e044184fb3ba1b7679fd0386291ed183782683d9517",
+    "zh:f7dd02f6d36844da23c9a27bb084503812c29c1aec4aba97237fec16860fdc8c",
   ]
 }
 
--- terraform/ecs-service.tf
+++ terraform/ecs-service.tf
@@ -1,63 +1,12 @@
-# IAM Role for ECS Service interaction with load balancer
-resource "aws_iam_role" "tomato_lb" {
-  name = "tomato-lb"
-
-  assume_role_policy = jsonencode({
-    Version = "2008-10-17"
-    Statement = [
-      {
-        Sid    = ""
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-
-}
-
-resource "aws_iam_role_policy" "tomato_lb" {
-  name = aws_iam_role.tomato_lb.name
-  role = aws_iam_role.tomato_lb.name
-
-  policy = jsonencode(
-    {
-      Version = "2012-10-17",
-      Statement = [
-        {
-          Effect = "Allow"
-          Action = [
-            "ec2:Describe*",
-            "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-            "elasticloadbalancing:DeregisterTargets",
-            "elasticloadbalancing:Describe*",
-            "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-            "elasticloadbalancing:RegisterTargets"
-          ]
-          Resource = "*"
-        }
-      ]
-  })
-}
-
-data "aws_iam_role" "ecs_task_role" {
-  name = "ecs-exec-task-role"
-}
-
-resource "aws_ecs_task_definition" "webapp" {
-  family        = "tomato-webapp"
-  task_role_arn = data.aws_iam_role.ecs_task_role.arn
+resource "aws_ecs_task_definition" "aggregator" {
+  family             = "aggregator"
+  task_role_arn      = data.aws_iam_role.ecs_task.arn
+  execution_role_arn = data.aws_iam_role.ecs_task.arn
 
   container_definitions = jsonencode(
     [
       {
-        volumesFrom       = []
-        extraHosts        = null
-        dnsServers        = null
-        disableNetworking = null
-        dnsSearchDomains  = null
+        name = "aggregator"
         portMappings = [
           {
             hostPort      = 0
@@ -65,290 +14,228 @@ resource "aws_ecs_task_definition" "webapp" {
             protocol      = "tcp"
           }
         ]
-        hostname              = null
-        essential             = true
-        entryPoint            = null
-        mountPoints           = []
-        name                  = "tomato"
-        ulimits               = null
-        dockerSecurityOptions = null
+        essential = true
+        dependsOn = [
+          {
+            "containerName" : "aggregator-init"
+            "condition" : "COMPLETE"
+          }
+        ]
         environment = [
           {
-            name  = "RDS_HOST"
-            value = aws_db_instance.tomato.address
+            name  = "GKEY"
+            value = var.GOOGLE_API_KEY
           },
           {
-            name  = "RDS_NAME"
-            value = aws_db_instance.tomato.db_name
+            name  = "AGGREGATOR_MODE_ENABLED"
+            value = "true"
           },
           {
-            name  = "RDS_USER"
-            value = aws_db_instance.tomato.username
+            name  = "NEW_UI_ENABLED"
+            value = "false"
           },
           {
-            name  = "RDS_PASSWORD"
-            value = aws_db_instance.tomato.password
+            name  = "DB_DATABASE"
+            value = "aggregator"
           },
           {
-            name  = "RDS_PORT"
-            value = tostring(aws_db_instance.tomato.port)
+            name  = "DB_USER"
+            value = "aggregator"
           },
           {
-            name  = "GOOGLE_MAPS_API_KEY"
-            value = "AIzaSyD4BPAvDHL4CiRcFORdoUCpqwVuVz1F9r8"
+            name  = "DB_PASSWORD"
+            value = var.rds_password
           },
           {
-            name  = "SECRET_KEY"
-            value = var.secret_key
+            name  = "DB_HOST"
+            value = aws_db_instance.aggregator.address
+          },
+          {
+            name  = "DB_PREFIX"
+            value = "na"
           }
         ]
-        workingDirectory       = "/code"
-        readonlyRootFilesystem = null
-        image                  = "${aws_ecr_repository.tomato.repository_url}:latest"
+        workingDirectory = "/tmp"
+        image            = "bmltenabled/bmlt-root-server:aggregator"
+        repositoryCredentials = {
+          credentialsParameter = data.aws_secretsmanager_secret.docker.arn
+        }
         command = [
-          "sh",
-          "-c",
-          "python3 manage.py initialize && uwsgi --ini /code/uwsgi.ini"
+          "/bin/bash",
+          "/tmp/start-bmlt.sh"
         ]
-        user         = null
-        dockerLabels = null
         logConfiguration = {
-          logDriver = "awslogs"
+          logDriver = "awslogs",
           options = {
-            awslogs-group         = aws_cloudwatch_log_group.tomato_webapp.name
+            awslogs-group         = aws_cloudwatch_log_group.aggregator.name
             awslogs-region        = "us-east-1"
-            awslogs-stream-prefix = "webapp"
+            awslogs-stream-prefix = "aggregator"
           }
         }
-        cpu               = 700
-        privileged        = null
-        memoryReservation = 512
+        memoryReservation = 1280
         linuxParameters = {
           initProcessEnabled = true
         }
-      }
-  ])
-
-}
-
-//resource "aws_ecs_task_definition" "webapp" {
-//  family = "tomato-webapp"
-//
-//  container_definitions = <<EOF
-//[
-//  {
-//    "volumesFrom": [],
-//    "extraHosts": null,
-//    "dnsServers": null,
-//    "disableNetworking": null,
-//    "dnsSearchDomains": null,
-//    "portMappings": [
-//      {
-//        "hostPort": 0,
-//        "containerPort": 8000,
-//        "protocol": "tcp"
-//      }
-//    ],
-//    "hostname": null,
-//    "essential": true,
-//    "entryPoint": null,
-//    "mountPoints": [],
-//    "name": "tomato",
-//    "ulimits": null,
-//    "dockerSecurityOptions": null,
-//    "environment": [
-//      {
-//        "name": "RDS_HOST",
-//        "value": "${aws_db_instance.tomato.address}"
-//      },
-//      {
-//        "name": "RDS_NAME",
-//        "value": "${aws_db_instance.tomato.name}"
-//      },
-//      {
-//        "name": "RDS_USER",
-//        "value": "${aws_db_instance.tomato.username}"
-//      },
-//      {
-//        "name": "RDS_PASSWORD",
-//        "value": "${aws_db_instance.tomato.password}"
-//      },
-//      {
-//        "name": "RDS_PORT",
-//        "value": "${aws_db_instance.tomato.port}"
-//      },
-//      {
-//        "name": "GOOGLE_MAPS_API_KEY",
-//        "value": "AIzaSyD4BPAvDHL4CiRcFORdoUCpqwVuVz1F9r8"
-//      },
-//      {
-//        "name": "SECRET_KEY",
-//        "value": "${var.secret_key}"
-//      },
-//      {
-//        "name": "CACHE_FORMATS",
-//        "value": "1"
-//      },
-//      {
-//        "name": "REDIS_LOCATION",
-//        "value": "redis://redis:6379"
-//      }
-//    ],
-//    "links": ["redis"],
-//    "workingDirectory": "/code",
-//    "readonlyRootFilesystem": null,
-//    "image": "${aws_ecr_repository.tomato.repository_url}:latest",
-//    "command": [
-//      "sh",
-//      "-c",
-//      "python3 manage.py initialize && uwsgi --ini /code/uwsgi.ini"
-//    ],
-//    "user": null,
-//    "dockerLabels": null,
-//    "logConfiguration": {
-//      "logDriver": "awslogs",
-//      "options": {
-//        "awslogs-group": "${aws_cloudwatch_log_group.tomato_webapp.name}",
-//        "awslogs-region": "us-east-1",
-//        "awslogs-stream-prefix": "webapp"
-//      }
-//    },
-//    "cpu": 700,
-//    "privileged": null,
-//    "memoryReservation": 512,
-//    "linuxParameters": {
-//      "initProcessEnabled": true
-//    }
-//  },
-//  {
-//    "name": "redis",
-//    "volumesFrom": [],
-//    "extraHosts": null,
-//    "dnsServers": null,
-//    "disableNetworking": null,
-//    "dnsSearchDomains": null,
-//    "portMappings": [
-//      {
-//        "containerPort": 6379,
-//        "protocol": "tcp"
-//      }
-//    ],
-//    "hostname": null,
-//    "essential": true,
-//    "entryPoint": ["docker-entrypoint.sh"],
-//    "mountPoints": [],
-//    "ulimits": null,
-//    "dockerSecurityOptions": null,
-//    "environment": [],
-//    "links": [],
-//    "workingDirectory": "/data",
-//    "readonlyRootFilesystem": null,
-//    "image": "redis:6.0.5",
-//    "command": ["redis-server"],
-//    "user": null,
-//    "dockerLabels": null,
-//    "logConfiguration": {
-//      "logDriver": "awslogs",
-//      "options": {
-//        "awslogs-group": "${aws_cloudwatch_log_group.tomato_webapp.name}",
-//        "awslogs-region": "us-east-1",
-//        "awslogs-stream-prefix": "redis"
-//      }
-//    },
-//    "memoryReservation": 128,
-//    "privileged": null,
-//    "linuxParameters": {
-//      "initProcessEnabled": true
-//    }
-//  }
-//]
-//EOF
-//
-//}
-
-resource "aws_ecs_task_definition" "tomato_root_server_import" {
-  family        = "tomato-root-server-import"
-  task_role_arn = data.aws_iam_role.ecs_task_role.arn
-
-  container_definitions = jsonencode(
-    [
+      },
       {
-        volumesFrom           = []
-        extraHosts            = null
-        dnsServers            = null
-        disableNetworking     = null
-        dnsSearchDomains      = null
-        portMappings          = []
-        hostname              = null
-        essential             = true
-        entryPoint            = null
-        mountPoints           = []
-        name                  = "tomato"
-        ulimits               = null
-        dockerSecurityOptions = null
-        environment = [
+        name = "aggregator-init"
+        portMappings = [
           {
-            name  = "RDS_HOST",
-            value = aws_db_instance.tomato.address
-          },
-          {
-            name  = "RDS_NAME",
-            value = aws_db_instance.tomato.db_name
-          },
-          {
-            name  = "RDS_USER",
-            value = aws_db_instance.tomato.username
-          },
-          {
-            name  = "RDS_PASSWORD",
-            value = aws_db_instance.tomato.password
-          },
-          {
-            name  = "RDS_PORT",
-            value = tostring(aws_db_instance.tomato.port)
+            hostPort      = 0
+            containerPort = 8000
+            protocol      = "tcp"
           }
         ]
-        links                  = []
-        workingDirectory       = "/code"
-        readonlyRootFilesystem = null
-        image                  = "${aws_ecr_repository.tomato.repository_url}:latest"
-        command = [
-          "sh",
-          "-c",
-          "python3 manage.py initialize && python3 manage.py import_root_servers"
+        essential = false
+        environment = [
+          {
+            name  = "GKEY"
+            value = var.GOOGLE_API_KEY
+          },
+          {
+            name  = "AGGREGATOR_MODE_ENABLED"
+            value = "true"
+          },
+          {
+            name  = "NEW_UI_ENABLED"
+            value = "false"
+          },
+          {
+            name  = "DB_DATABASE"
+            value = "aggregator"
+          },
+          {
+            name  = "DB_USER"
+            value = "aggregator"
+          },
+          {
+            name  = "DB_PASSWORD"
+            value = var.rds_password
+          },
+          {
+            name  = "DB_HOST"
+            value = aws_db_instance.aggregator.address
+          },
+          {
+            name  = "DB_PREFIX"
+            value = "na"
+          }
         ]
-        user         = null
-        dockerLabels = null
+        workingDirectory = "/tmp"
+        image            = "bmltenabled/bmlt-root-server:aggregator"
+        repositoryCredentials = {
+          credentialsParameter = data.aws_secretsmanager_secret.docker.arn
+        },
+        command = [
+          "/bin/bash",
+          "/tmp/aggregator-initialize-database.sh"
+        ]
         logConfiguration = {
           logDriver = "awslogs"
           options = {
-            awslogs-group         = aws_cloudwatch_log_group.tomato_root_server_import.name
+            awslogs-group         = aws_cloudwatch_log_group.aggregator_init.name
             awslogs-region        = "us-east-1"
-            awslogs-stream-prefix = "daemon"
+            awslogs-stream-prefix = "init"
           }
         }
-        cpu               = 256
-        privileged        = null
         memoryReservation = 256
         linuxParameters = {
           initProcessEnabled = true
         }
       }
-  ])
+    ]
+  )
 }
 
-resource "aws_ecs_service" "webapp" {
-  name                               = "webapp"
-  cluster                            = aws_ecs_cluster.main.id
+resource "aws_ecs_task_definition" "aggregator_import" {
+  family             = "aggregator-import"
+  task_role_arn      = data.aws_iam_role.ecs_task.arn
+  execution_role_arn = data.aws_iam_role.ecs_task.arn
+
+  container_definitions = jsonencode(
+    [
+      {
+        name = "aggregator-import"
+        portMappings = [
+          {
+            hostPort      = 0
+            containerPort = 8000
+            protocol      = "tcp"
+          }
+        ]
+        essential = true
+        environment = [
+          {
+            name  = "GKEY"
+            value = var.GOOGLE_API_KEY
+          },
+          {
+            name  = "AGGREGATOR_MODE_ENABLED"
+            value = "true"
+          },
+          {
+            name  = "NEW_UI_ENABLED"
+            value = "false"
+          },
+          {
+            name  = "DB_DATABASE"
+            value = "aggregator"
+          },
+          {
+            name  = "DB_USER"
+            value = "aggregator"
+          },
+          {
+            name  = "DB_PASSWORD"
+            value = var.rds_password
+          },
+          {
+            name  = "DB_HOST"
+            value = aws_db_instance.aggregator.address
+          },
+          {
+            name  = "DB_PREFIX"
+            value = "na"
+          }
+        ]
+        workingDirectory = "/tmp"
+        image            = "bmltenabled/bmlt-root-server:aggregator"
+        repositoryCredentials = {
+          credentialsParameter = data.aws_secretsmanager_secret.docker.arn
+        }
+        command = [
+          "/bin/bash",
+          "/tmp/aggregator-import-root-servers.sh"
+        ]
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = aws_cloudwatch_log_group.aggregator_import.name
+            awslogs-region        = "us-east-1"
+            awslogs-stream-prefix = "daemon"
+          }
+        }
+        memoryReservation = 256
+        linuxParameters = {
+          initProcessEnabled = true
+        }
+      }
+    ]
+  )
+}
+
+resource "aws_ecs_service" "aggregator" {
+  name                               = "aggregator"
+  cluster                            = aws_ecs_cluster.aggregator.id
   desired_count                      = 2
-  iam_role                           = aws_iam_role.tomato_lb.name
-  task_definition                    = aws_ecs_task_definition.webapp.arn
+  iam_role                           = data.aws_iam_role.ecs_service.name
+  task_definition                    = aws_ecs_task_definition.aggregator.arn
   enable_execute_command             = true
   deployment_minimum_healthy_percent = 50
 
   load_balancer {
-    target_group_arn = aws_alb_target_group.tomato.id
-    container_name   = "tomato"
+    target_group_arn = aws_lb_target_group.aggregator.id
+    container_name   = "aggregator"
     container_port   = 8000
   }
 
@@ -357,8 +244,7 @@ resource "aws_ecs_service" "webapp" {
     field = "attribute:ecs.availability-zone"
   }
 
-  depends_on = [
-    aws_iam_role_policy.tomato_lb,
-    aws_alb_listener.tomato_https,
-  ]
+  #  lifecycle {
+  #    ignore_changes = [task_definition]
+  #  }
 }
